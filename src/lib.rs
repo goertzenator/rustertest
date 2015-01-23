995b9f2cf484cd753e2818adb95ec8ad5370456c
Daniel Goertzen <daniel.goertzen@gmail.com>
2015-01-23 13:11:13 -0600

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,27 @@
 extern crate ruster;
+extern crate core;
 
 use ruster::raw::*;
 
+use core::marker::Sync;
 
+// #[no_mangle]
+// pub extern "C" fn native_add() -> ERL_NIF_TERM
+// {
+//     //let x = unsafe {enif_make_badarg(env)};
+ 
+//     let x:c_int = 0; //ERL_NIF_TERM(0);
+//     x 
+
+// }
 
 #[no_mangle]
 pub extern "C" fn native_add(env: *mut ErlNifEnv,
                              argc: c_int,
                              args: *const ERL_NIF_TERM) -> ERL_NIF_TERM
 {
-/*    if argc != 2
+
+    if argc != 2
         { unsafe { return enif_make_badarg(env); } }
     let mut a: c_int = 0;
     let mut b: c_int = 0;
@@ -20,67 +32,74 @@ pub extern "C" fn native_add(env: *mut ErlNifEnv,
             { return enif_make_badarg(env); }
         enif_make_int(env, a + b)
     }
-    */
-    ERL_NIF_TERM
 }
 
-static funcs: [ErlNifFunc] = [
+
+
+// pub extern "C" fn init() -> *const MyStruct<'static> {
+//     static ENTRY: MyStruct<'static> =
+//         MyStruct { name: "hello\0" };
+//     &ENTRY
+// }
+
+
+// // this is ok
+// static raw_string: *const str = "why!!!\0";
+
+
+// pub struct MyStruct {
+//     pub name: *const str,
+// }
+
+// unsafe impl Sync for MyStruct {}
+
+// static mystruct: MyStruct = MyStruct {name: "why!!!\0"};
+
+// src/lib.rs:52:29: 52:56 error: the trait `core::marker::Sync` is not implemented for the type `*const str`
+// src/lib.rs:52 static mystruct: MyStruct = MyStruct {name: "why!!!\0"};
+//                                           ^~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+// struct MyStruct<'a> {
+//     name: (*const)'static str,
+// }
+// static mystruct: MyStruct<'static> = MyStruct {name: "why!!!\0"};
+
+
+// static mytest: ErlNifFunc = ErlNifFunc {
+//     name:     "native_add\0",
+//     arity:    2,
+//     function: native_add,
+//     flags:    0,
+// };
+
+
+static FUNCS: [ErlNifFunc; 1] = [
     ErlNifFunc{ name:     "native_add\0",
                 arity:    2,
-                function: native_add }
+                function: native_add,
+                flags:    0,
+              }
 ];
-/*
-static mut nif_entry: ErlNifEntry = ErlNifEntry{
-    major           : 2,
-    minor           : 6,
-    name            : 0 as *const c_char,
-    num_of_funcs    : 0,
-    funcs           : 0 as *mut ErlNifFunc,
-    load            : None,
-    reload          : None,
-    upgrade         : None,
-    unload          : None,
-    vm_variant      : 0 as *const c_char
-};
-
 
 #[no_mangle]
-pub extern "C" fn nif_init() -> *mut ErlNifEntry
-{
-    unsafe {
-        funcs[0].name = CString::from_slice("native_add".as_bytes()).as_ptr();
-        nif_entry.name = CString::from_slice("er".as_bytes()).as_ptr();
-        nif_entry.num_of_funcs = funcs.len() as i32;
-        nif_entry.funcs = funcs.as_mut_ptr();
-        nif_entry.vm_variant = CString::from_slice("beam.vanilla".as_bytes()).as_ptr();
-        &mut nif_entry
-    }
-}
-*/
-
-
-
-
-pub extern "C" fn nif_init() -> *mut ErlNifEntry
-{
-	static mut entry: ErlNifEntry = ErlNifEntry{
+pub extern "C" fn nif_init() -> *const ErlNifEntry {
+	static ENTRY: ErlNifEntry = ErlNifEntry{
 		major : NIF_MAJOR_VERSION,
 		minor : NIF_MINOR_VERSION,
 		name : "rustnif\0",
-		num_of_funcs : 0,
-		funcs : 0 as *mut ErlNifFunc,
+		num_of_funcs : 1,
+		funcs : &FUNCS,
 		load :    None,
 		reload :  None,
 		upgrade : None,
 		unload :  None,
 		vm_variant : "beam.vanilla\0",
+        options: 0,
 	};
-	// unsafe {
-	// 	funcs[0].name = CString::from_slice("native_add".as_bytes()).as_ptr();
-	// 	entry.name = CString::from_slice("er".as_bytes()).as_ptr();
-	// 	entry.num_of_funcs = funcs.len() as i32;
-	// 	entry.funcs = funcs.as_mut_ptr();
-	
-	// }
-	unsafe{ &mut entry }
+
+    &ENTRY
 }
